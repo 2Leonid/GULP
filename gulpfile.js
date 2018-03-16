849a7a7289d39d1b58cae223f5d3ec82d7b7ac51
2Leonid <leon.pavlenok@yandex.ru>
2018-03-16 21:02:21 +0300

    spase

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var concat = require('gulp-concat');
 var useref = require('gulp-useref');
-var	gulpif = require('gulp-if');
+var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-clean-css');
 
 gulp.task('union',['html'], function(){
-	return gulp.src('app/**/*.html')
-		.pipe(concat('all.html'))
-		.pipe(gulp.dest('dist/html/'));
+    return gulp.src('app/**/*.html')
+        .pipe(concat('all.html'))
+        .pipe(gulp.dest('dist/html/'));
 });
 
 gulp.task('html', function () {
@@ -22,20 +22,20 @@ gulp.task('html', function () {
 });
 
 gulp.task('sass-compile', function(){
-	return gulp.src('app/scss/**/*.scss')
-	.pipe(sass()) 
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.reload({
-		stream: true
-	}));
+    return gulp.src('app/scss/**/*.scss')
+    .pipe(sass()) 
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
 });
 
 gulp.task('browserSync', function(){
-	browserSync({
-		server:{baseDir:'app'}
-	})
+    browserSync({
+        server:{baseDir:'app'}
+    })
 });
 
 gulp.task('watch', ['browserSync'], function (){
-	gulp.watch('app/scss/**/*.scss', ['sass-compile']);
+    gulp.watch('app/scss/**/*.scss', ['sass-compile']);
 })
